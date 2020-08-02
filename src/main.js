1238b94b57e66dafb2bbb723f1a0fc6cd5884b47
hesshess <64644262+hesshess@users.noreply.github.com>
2020-08-02 22:27:43 +0100

    display items
    JS

--- src/main.js
+++ src/main.js
@@ -5,11 +5,26 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
+
+// Create HTML list from the given item data
+function createHTMLString(item) {
+  return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
 // main
 loadItems()
   .then((items) => {
-    console.log(items);
-    //   displayItems(items);
+    displayItems(items);
     //   setEventListeners(itmes)
   })
   .catch(console.log);
